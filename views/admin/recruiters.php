808009e0166ef0c8e028dbc0fdf09160dcc549f8
adel141 <mithilahakim7@gmail.com>
2026-05-17 13:36:21 +0600

    recruiters view added same as employee and seekers

---
+++ views/admin/recruiters.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Admin | Recruiters</title>
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <script src="../../public/js/admin.js"></script>
+</head>
+<body>
+    <div class="card flush">
+    <table class="tbl">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Registered</th>
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody id="user-table-body">
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+</body>
+<script >
+
+
+</script>
+</html>
